ad350c1f8224057f86f3b576accaec0d683d3a8c
IlyaKavko <58006263+IlyaKavko@users.noreply.github.com>
2020-10-01 22:32:32 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,25 +1,42 @@
-const total = $("#total");
-
-$("#sell").on("click", function () {
+checkPrice(); // Checking for content
+sum(); //Checking for sum
+//  display discount
+$("#discount").on("click", function () {
   if ($(this).is(":checked")) {
-    $(".sell").css("display", "block");
+    $(".discount").css("display", "block");
   } else {
-    $(".sell").css("display", "none");
+    $(".discount").css("display", "none");
   }
 });
-
+// Blocks transition by clicking on the plus
 $(".nolink > span").on("click", function () {
-  return false; // это отменит переход по ссылке.
+  return false;
 });
+// calculating the amount
 $("input").focus(function () {
   let perant = $(this).parent("td").parent("tr").attr("id");
   $("body").on("input", "input[type=text]", function () {
     let price = $(`#price-${perant}`).val();
     let quantity = $(`#quantity-${perant}`).val();
-    let result = price * quantity;
-    $(`#total-${perant}`).val(result);
-    sum();
+    let discount = $(`#discount-${perant}`).val();
+    //calculation of discounts
+    if (discount === "") {
+      let result = price * quantity;
+      $(`#total-${perant}`).val(result);
+      sum();
+    } else {
+      let result = (price * quantity * discount) / 100;
+      $(`#total-${perant}`).val(result);
+      sum();
+    }
   });
+});
+// deleting an item
+$(".fa-times").on("click", function () {
+  let perant = $(this).parent("td").parent("tr").attr("id");
+  $(`tr[id=${perant}]`).remove();
+  sum();
+  checkPrice();
 });
 
 function sum() {
@@ -30,22 +47,8 @@ function sum() {
   $(".alert").find("span").html(sum);
 }
 
-$(".fa-times").on("click", function () {
-  let perant = $(this).parent("td").parent("tr").attr("id");
-  $(`tr[id=${perant}]`).remove();
-  sum();
-  checkPrice();
-});
-
 function checkPrice() {
   if (!$("tbody").children().length) {
     $(".empty").css("display", "block");
   }
 }
-
-$(".name").each(function () {
-  let str = this.innerText;
-  console.log(str.slice(0, 20));
-  let length = str.slice(0, 20);
-  $(this).find(".filter").html(`${length}...`);
-});
